92818fe433d6702302ed48bc10f5db9e347767cc
Timothy Morales <timothydanielmorales@gmail.com>
2023-05-03 00:20:13 -0500

    undo and redo functionality done

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ interface Dots {
 
 function App() {
   const [dots, setDots] = useState<Dots[]>([])
+  const [cache, setCache] = useState<Dots[]>([])
 
   const draw = (e: MouseEvent) =>{
     setDots([...dots,  {
@@ -16,15 +17,34 @@ function App() {
     }])
   }
 
-  // const undo = () => {
+  const undo = () => {
+    if(dots.length > 0){
+      const newDots = [...dots]
+      const lastDot = newDots.pop() as Dots
+      Promise.all([
+        setCache([...cache, lastDot]),
+        setDots(newDots)
+      ])
+    }
+  }
 
-  // }
+  const redo = () => {
+    if(cache.length > 0){
+      const newCache = [...cache]
+      const lastCacheItem = newCache.pop() as Dots
+      Promise.all([
+        setDots([...dots, lastCacheItem]),
+        setCache(newCache)
+      ])
+    }
+
+  }
 
   return (
    <div className="App">
     <div id="button-wrapper">
-      <button>Undo</button>
-      <button>Redo</button>
+      <button onClick={undo}>Undo</button>
+      <button onClick={redo}>Redo</button>
     </div>
     <div id="click-area" onClick={draw}>
       {dots.map(({x, y}: Dots, i: number) => (
